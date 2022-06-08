d90dc71fe3793e0e094be571dfd99bb979d995a0
Stan <vaqxai@gmail.com>
2022-06-08 23:36:06 +0200

    add knapsack problem algo

--- src/knapsack_item.rs
+++ src/knapsack_item.rs
@@ -3,15 +3,22 @@ use std::fmt::Formatter;
 use std::fmt::Display;
 use std::fmt::Error;
 use std::str::FromStr;
+use std::cell::Cell;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 pub struct KnapsackItem {
+    id: usize,
     value: i32,
     weight: i32,
 }
 
+static ID: AtomicUsize = AtomicUsize::new(0);
+
 impl KnapsackItem {
     pub fn new(value: i32, weight: i32) -> KnapsackItem {
+
         KnapsackItem {
+            id: ID.fetch_add(1, Ordering::SeqCst),
             value,
             weight,
         }
@@ -36,13 +43,13 @@ impl KnapsackItem {
 
 impl Debug for KnapsackItem {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "KnapsackItem {{ value: {}, weight: {} }}", self.value, self.weight)
+        write!(f, "#{}(V: {} W: {})", self.id, self.value, self.weight)
     }
 }
 
 impl Display for KnapsackItem {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "KnapsackItem {{ value: {}, weight: {} }}", self.value, self.weight)
+        write!(f, "#{}(V: {} W: {})", self.id, self.value, self.weight)
     }
 }
 
@@ -56,5 +63,11 @@ impl FromStr for KnapsackItem {
         let weight = parts.next().ok_or("No weight found".to_string())?;
         let weight = weight.parse::<i32>().map_err(|e| e.to_string())?;
         Ok(KnapsackItem::new(value, weight))
+    }
+}
+
+impl PartialEq for KnapsackItem {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::{Parser, Subcommand};
 
 mod knapsack_item;
+mod knapsack;
+use itertools::{Permutations, Itertools, Powerset};
+use std::{slice::Iter, iter::Filter};
 use knapsack_item::KnapsackItem;
 
 #[derive(Parser)]
@@ -27,15 +30,71 @@ enum Commands {
     }
 }
 
+fn get_permutation_iter(items:&Vec<KnapsackItem>) -> (Powerset<Iter<KnapsackItem>>, usize) {
+    let iter : Iter<KnapsackItem> = items.into_iter();
+    (iter.powerset(), items.into_iter().powerset().count())
+}
+
+fn reject_above_weight_limit<'a>(items_iter: Powerset<Iter<'a, KnapsackItem>>, knapsack_size: &usize) -> Vec<Vec<&'a KnapsackItem>> {
+    items_iter.filter(|items| {
+        let total_weight: i32 = items.iter().map(|item| item.get_weight()).sum();
+        total_weight <= (*knapsack_size as i32)
+    }).collect()
+}
+
+fn sum_values(items: &Vec<&KnapsackItem>) -> i32 {
+    items.iter().map(|item| item.get_value()).sum()
+}
+
+fn sum_weights(items: &Vec<&KnapsackItem>) -> i32 {
+    items.iter().map(|item| item.get_weight()).sum()
+}
+
+fn get_sorted_by_value<'a>(items: Vec<Vec<&'a KnapsackItem>>) -> Vec<Vec<&'a KnapsackItem>> {
+    items.into_iter().sorted_by(|a, b| sum_values(&a).cmp(&sum_values(&b))).collect()
+}
+
+fn get_knapsack_signature(all_items: &Vec<KnapsackItem>, chosen_items: &Vec<&KnapsackItem>) -> String {
+    let mut result = String::new();
+    for item in all_items {
+        if chosen_items.contains(&item) {
+            result.push_str("1");
+        } else {
+            result.push_str("0");
+        }
+    }
+    result
+}
+
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
         Commands::Solve { knapsack_size, items } => {
+
+            let time_begun = std::time::Instant::now();
+
             for item in items {
                 println!("{}", item);
             }
-            panic!("Not implemented");
+            
+            let (items_iter, items_count) = get_permutation_iter(items);
+            let filtered_items = reject_above_weight_limit(items_iter, knapsack_size);
+            let sorted_items = get_sorted_by_value(filtered_items);
+
+            let time_ended = time_begun.elapsed();
+
+            for chosen_items in sorted_items {
+                let signature = get_knapsack_signature(&items, &chosen_items);
+                println!("-----------------");
+                println!("({}) \t {:?}", signature, chosen_items);
+                let total_weight = sum_weights(&chosen_items);
+                println!("Total value: {} \t Total weight: {} \t Remaining space: {}", sum_values(&chosen_items), total_weight, *knapsack_size as i32 - total_weight);
+            }
+
+            println!("Total number of unique combinations: {}", items_count);
+            println!("Time taken: {:?}, with printing: {:?}", time_ended, time_begun.elapsed());
+
         },
         Commands::FromFile { knapsack_size, filename } => {
             panic!("Not implemented");
